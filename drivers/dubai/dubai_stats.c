cc1d115c3ab9dfd7db136bb7a26f1549c028a79b
JBolho <hugo.ss.android@gmail.com>
2018-10-15 12:10:38 +0100

    D.U.B.A.I: Make it useless.
    Since we can't just disable it, make it unreachable and potentially useless.

--- drivers/dubai/dubai_stats.c
+++ drivers/dubai/dubai_stats.c
@@ -28,10 +28,10 @@
 #include <chipset_common/dubai/dubai.h>
 #include <chipset_common/dubai/dubai_common.h>
 
-#define KWORKER_HASH_BITS			(10)
-#define MAX_SYMBOL_LEN				(48)
-#define MAX_DEVPATH_LEN				(128)
-#define PRINT_MAX_LEN				(40)
+#define KWORKER_HASH_BITS			(2)
+#define MAX_SYMBOL_LEN				(2)
+#define MAX_DEVPATH_LEN				(2)
+#define PRINT_MAX_LEN				(2)
 #define MAX_BRIGHTNESS				(10000)
 #define BINDER_STATS_HASH_BITS		(10)
 #define DUBAI_AOD_DURATION_ENENT	(6)
